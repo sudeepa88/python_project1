34a97adb04564d2ea4ac95e6a6a4a5f94a96fbe8
sudeepa pal <sudeepapal@sudeepas-MacBook-Air.local>
2022-01-05 20:45:01 +0530

    first commit

---
+++ project1.py
@@ -0,0 +1,11 @@
+students = []
+ 
+
+n = int(input("Enter number of cities : "))
+ 
+print("Enter names of cities: ")
+for i in range(0, n):
+    x=input()
+    students.append(x)
+     
+print(students)
